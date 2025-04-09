a1e695214eb255746238762c96fe30a3477e0c6e
Ghazyxs <rafi.ghazy8@gmail.com>
2025-04-09 12:46:32 +0700

    tambah projek

--- percobaan/test.cpp
+++ percobaan/test.cpp
@@ -4,5 +4,8 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    cout << "This is a test program." << endl;
+    cout << "Let's see if it works." << endl;
     return 0;
+
 }
