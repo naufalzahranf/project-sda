f9dd9672e01ab4ed9397d279d49bb23f3694f976
felixskdeleton <naufalzahran1979@gmail.com>
2025-04-09 12:59:03 +0700

    test

--- percobaan/test.cpp
+++ percobaan/test.cpp
@@ -6,6 +6,7 @@ int main() {
     cout << "Hello, World!" << endl;
     cout << "This is a test program." << endl;
     cout << "Let's see if it works." << endl;
+    cout << "This is a test for the GitHub Copilot." << endl;
     return 0;
 
 }
