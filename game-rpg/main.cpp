6e1cc8b252577243316e01978f88d2c4d2c828d0
fjribgs <fajribagaskara@upi.edu>
2025-06-10 11:56:50 +0700

    test

--- game-rpg/main.cpp
+++ game-rpg/main.cpp
@@ -2,6 +2,46 @@
 #include <string>
 using namespace std;
 
+struct treeRuangan
+{
+    string namaRuangan;
+    string deskripsi;
+    bool adaMonster;
+    bool adaHarta;
+    treeRuangan *kiri, *kanan;
+};
+
+treeRuangan *pohonRuangan, *rootRuangan;
+
+void deklarasiTree()
+{
+    pohonRuangan = NULL;
+}
+
+void insertTree(treeRuangan **rootRuangan, string nama, string deskripsi, bool monster, bool harta)
+{
+    if (*rootRuangan == NULL)
+    {
+        treeRuangan* nodeBaru = new treeRuangan;
+        nodeBaru -> namaRuangan = nama;
+        nodeBaru -> deskripsi = deskripsi;
+        nodeBaru -> adaHarta = harta;
+        nodeBaru -> adaMonster = monster;
+        nodeBaru -> kiri = nodeBaru -> kanan = NULL;
+        *rootRuangan = nodeBaru;
+        return;
+    }
+
+    if (nama.length() < (*rootRuangan) -> namaRuangan.length())
+    {
+        insertTree(&(*rootRuangan) -> kiri, nama, deskripsi, monster, harta);
+    }
+    else
+    {
+        insertTree(&(*rootRuangan)-> kanan, nama, deskripsi, monster, harta);
+    }
+}
+
 int main()
 {
     cout << "Test" << endl;
